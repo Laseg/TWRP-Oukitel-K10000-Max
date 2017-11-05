1ccb43cef8ce305c743540f4fc6324ee6d1a621d
Laseg <lasjackwolf@gmail.com>
2017-11-05 23:27:03 +0300

    32-bit ver

--- K10000_Max/omni_K10000_Max.mk
+++ K10000_Max/omni_K10000_Max.mk
@@ -2,6 +2,7 @@
 $(call inherit-product, device/Oukitel/K10000_Max/K10000_Max.mk)
 
 # Device identifier. This must come after all inclusions
+PRODUCT_RELEASE_NAME := Laseg
 PRODUCT_DEVICE := K10000_Max
 PRODUCT_NAME := omni_K10000_Max
 PRODUCT_BRAND := OUKITEL
